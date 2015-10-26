2fd4e081e777e017e8aac5dc1a2daf8515af4fe7
Shubo Yang <yangshubo@outlook.com>
2015-10-26 11:29:35 +0800

    init

---
+++ connection-memoryleak-test/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace connection_memoryleak_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int total_count = 100000;
+            int cur_count = 0;
+
+            while(cur_count < total_count)
+            {
+                cur_count++;
+
+                Socket sock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                sock.Connect(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 90));
+
+                Thread.Sleep(1000);
+
+                sock.Disconnect(true);
+                sock.Close();
+                sock = null;
+            }
+        }
+    }
+}
